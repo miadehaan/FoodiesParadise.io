715437a007ae6b95d29eff68397081f41f9afb8a
AndyMMunro <agovitadventures@gmail.com>
2020-10-17 10:52:13 -0700

    added cookie parser changed file names to datatables.js and dataControler.js

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require("express");
-
+var cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-
+var app = express()
+app.use(cookieParser())
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 // Set Handlebars.
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
@@ -23,7 +28,7 @@ var routes = require("./controllers/burgersController.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
