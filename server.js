7153f10b7d2c3108d964fb833c6848e9c334e600
AndyMMunro <agovitadventures@gmail.com>
2020-10-17 10:57:50 -0700

    added cookie parser changed file names to datatables.js and dataControler.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 8080;
 
 const app = express();
-var app = express()
+
+
 app.use(cookieParser())
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
