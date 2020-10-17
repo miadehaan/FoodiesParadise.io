4766c3c50c05fbc33fcfbc1047cca54bb715bcad
AndyMMunro <agovitadventures@gmail.com>
2020-10-17 13:52:54 -0700

    added find one post and delet to dish-routes.js

--- routes/dish-routes.js
+++ routes/dish-routes.js
@@ -7,12 +7,41 @@ const {
 
 const app = express();
 
-app.get("/api/dish", function (req, res) {
+
+app.get("/dish", function (req, res) {
     db.Dish.findAll({
         include: [db.Post]
     }).then(function (dbDish) {
         res.json(dbDish)
     });
 });
+
+app.get("/dish/:id", function (req, res) {
+    db.Dish.findOne({
+        where: {
+            id: req.pramas.id
+        },
+        include: [db.Post]
+    }).then(function (dbDish) {
+        res.json(dbDish);
+    });
+});
+
+app.post("/dish", function (req, res) {
+    db.Dish.create(req.body).then(function (dbDish) {
+        res.json(dbDish);
+    });
+});
+
+app.delete("/dish/:id", function (req, res) {
+    db.Dish.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function (dbDish) {
+        res.json(dbDish);
+    })
+})
+
 
 module.exports = Router;
