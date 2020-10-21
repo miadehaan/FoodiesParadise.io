702e53012eeed9df2eac6d5265c70a0388917aed
nosam1998 <nosam1998@gmail.com>
2020-10-20 18:45:12 -0700

    Added Find Cookie Function

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -58,6 +58,12 @@ $(function () {
     geoFindMe();
 });
 
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
 // AJAX Call for YELP API
 function yelpAPI(latitude, longitude) {
     // AJAX call to Yelp Fusion - REST API
